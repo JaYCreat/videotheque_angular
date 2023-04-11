af2f9158066c7f1058bb0f40edd55ba8677db217
Justin Ravat <justin.ravat@protonmail.com>
2023-04-11 14:44:39 +0200

    restructuration orientée composants + ajout header/footer + refacto composant affiche liste films

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 // Angular CLI importe directement notre composant
 import { ListeFilmsComponent } from './liste-films/liste-films.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AffichageListeFilmsComponent } from './affichage-liste-films/affichage-liste-films.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Avant de le déclarer obligatoirement comme module
-    ListeFilmsComponent
+    ListeFilmsComponent,
+    AffichageListeFilmsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
